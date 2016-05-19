23a339c2fd066ab62ea15a9a4a198fc052e54d7a
Arthur Bulan <mrbulan@uw.edu>
2016-05-19 14:03:55 -0700

    K means algorithm
    Basic structure laid out

---
+++ app/src/main/java/com/something/yellowplanet/Cluster.java
@@ -0,0 +1,60 @@
+package com.something.yellowplanet;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Arthur on 5/17/16.
+ */
+public class Cluster {
+
+    public List points;
+    public Point centroid;
+    public int id;
+
+    //Creates a new Cluster
+    public Cluster(int id) {
+        this.id = id;
+        this.points = new ArrayList();
+        this.centroid = null;
+    }
+
+    public List getPoints() {
+        return points;
+    }
+
+    public void addPoint(Point point) {
+        points.add(point);
+    }
+
+    public void setPoints(List points) {
+        this.points = points;
+    }
+
+    public Point getCentroid() {
+        return centroid;
+    }
+
+    public void setCentroid(Point centroid) {
+        this.centroid = centroid;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void clear() {
+        points.clear();
+    }
+
+    public void plotCluster() {
+        System.out.println("[Cluster: " + id+"]");
+        System.out.println("[Centroid: " + centroid + "]");
+        System.out.println("[Points: \n");
+        for(Point p : points) {
+            System.out.println(p);
+        }
+        System.out.println("]");
+    }
+
+}
---
+++ app/src/main/java/com/something/yellowplanet/KMeans.java
@@ -0,0 +1,151 @@
+package com.something.yellowplanet;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.something.yellowplanet.Point;
+
+/**
+ * Created by Arthur on 5/17/16.
+ */
+public class KMeans {
+
+    private int NUM_CLUSTERS = 3;
+
+    private int NUM_POINTS = 15;
+
+    private static final int MIN_COORDINATE = 0;
+    private static final int MAX_COORDINATE = 10;
+
+    private List points;
+    private List clusters;
+
+    public KMeans() {
+        this.points = new ArrayList();
+        this.clusters = new ArrayList();
+    }
+
+    public static void main(String [] args)
+    {
+        KMeans kmeans = new KMeans();
+        kmeans.init();
+        kmeans.calculate();
+    }
+
+    public void init() {
+        points = Point.createRandomPoints(MIN_COORDINATE,MAX_COORDINATE,NUM_CLUSTERS);
+
+
+        for(int i = 0; i < NUM_CLUSTERS; i++) {
+            Cluster cluster = new Cluster();
+            Point centroid = Point.createRandomPoint(MIN_COORDINATE,MAX_COORDINATE);
+            cluster.setCentroid(centroid);
+            clusters.add(cluster);
+        }
+
+        plotClusters();
+    }
+
+    private void plotClusters() {
+        for (int i = 0; i < NUM_CLUSTERS; i++) {
+            Cluster c = clusters.get(i);
+            c.plotCluster();
+        }
+    }
+
+    public void calculate() {
+        boolean finish = false;
+        int iteration = 0;
+
+        // Add in new data, one at a time, recalculating centroids with each new one.
+        while(!finish) {
+            //Clear cluster state
+            clearClusters();
+
+            List lastCentroids = getCentroids();
+
+            //Assign points to the closer cluster
+            assignCluster();
+
+            //Calculate new centroids.
+            calculateCentroids();
+
+            iteration++;
+
+            List currentCentroids = getCentroids();
+
+            //Calculates total distance between new and old Centroids
+            double distance = 0;
+            for(int i = 0; i < lastCentroids.size(); i++) {
+                distance += Point.distance(lastCentroids.get(i),currentCentroids.get(i));
+            }
+            System.out.println("#################");
+            System.out.println("Iteration: " + iteration);
+            System.out.println("Centroid distances: " + distance);
+            plotClusters();
+
+            if(distance == 0) {
+                finish = true;
+            }
+        }
+    }
+
+    private void clearClusters() {
+        for(Cluster cluster : clusters) {
+            cluster.clear();
+        }
+    }
+
+    private List getCentroids() {
+        List centroids = new ArrayList(NUM_CLUSTERS);
+        for(Cluster cluster : clusters) {
+            Point aux = cluster.getCentroid();
+            Point point = new Point(aux.getX(),aux.getY());
+            centroids.add(point);
+        }
+        return centroids;
+    }
+
+    private void assignCluster() {
+        double max = Double.MAX_VALUE;
+        double min = max;
+        int cluster = 0;
+        double distance = 0.0;
+
+        for(Point point : points) {
+            min = max;
+            for(int i = 0; i < NUM_CLUSTERS; i++) {
+                Cluster c = clusters.get(i);
+                distance = Point.distance(point, c.getCentroid());
+                if(distance < min){
+                    min = distance;
+                    cluster = i;
+                }
+            }
+            point.setCluster(cluster);
+            clusters.get(cluster).addPoint(point);
+        }
+    }
+
+    private void calculateCentroids() {
+        for(Cluster cluster : clusters) {
+            double sumX = 0;
+            double sumY = 0;
+            List list = cluster.getPoints();
+            int n_points = list.size();
+
+            for(Point point : list) {
+                sumX += point.getX();
+                sumY += point.getY();
+            }
+
+            Point centroid = cluster.getCentroid();
+            if(n_points > 0) {
+                double newX = sumX / n_points;
+                double newY = sumY / n_points;
+                centroid.setX(newX);
+                centroid.setY(newY);
+            }
+        }
+    }
+}
